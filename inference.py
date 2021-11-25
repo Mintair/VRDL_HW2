08d3d43c253392c8446425242fe7dc7ddff38655
Fu Chuan Kai <cossetannie@gmail.com>
2021-11-26 00:01:02 +0800

    Add files via upload

--- inference.py
+++ inference.py
@@ -42,8 +42,6 @@ class MyDataset(Dataset):
         return self.transform(img)
 
 
-
-
 if __name__ == '__main__':
     submission = []
     net = torch.load(PATH)
@@ -51,20 +49,20 @@ if __name__ == '__main__':
     test_img_list = []
     output_list = {}
     test_img_list = os.listdir("test/test")
-    
+
     test_loader = DataLoader(MyDataset("test/test/",
                                        test_img_list,
                                        transform_test),
-                                         batch_size=1,
-                                         shuffle=False,
-                                         num_workers=0)
-    
+                             batch_size=1,
+                             shuffle=False,
+                             num_workers=0)
+
     with torch.no_grad():
         net.eval()
         for i, img in enumerate(test_loader, 0):
             pred = net(img)
             output_list[test_img_list[i]] = pred[0]
-        
+
     for img_name in test_img_list:
         print(img_name)
         # the image_name is as same as the image_id
@@ -76,14 +74,14 @@ if __name__ == '__main__':
 
             # An integer to identify the image
             det_box_info["image_id"] = image_id
-        
+
             # A list ( [left_x, top_y, width, height] )
             left = output_list[img_name]["boxes"][box][0].item()
             top = output_list[img_name]["boxes"][box][1].item()
             width = output_list[img_name]["boxes"][box][2].item() - left
             height = output_list[img_name]["boxes"][box][3].item() - top
             det_box_info["bbox"] = [left, top, width, height]
-            # A float number between 0 ~ 1 which means the confidence of the bbox
+            # Float number between 0 ~ 1 which means the confidence of the bbox
             det_box_info["score"] = output_list[img_name]["scores"][box].item()
 
             # An integer which means the label class
@@ -91,13 +89,8 @@ if __name__ == '__main__':
             print(det_box_info)
             submission.append(det_box_info)
 
-    # Write the list to answer.json 
+    # Write the list to answer.json
     json_object = json.dumps(submission, indent=4)
 
     with open("answer.json", "w") as outfile:
         outfile.write(json_object)
-
-
-
-
-
